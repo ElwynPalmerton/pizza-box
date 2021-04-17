09bf507689595e4951dcec44cd978656e1426c2e
Elwyn Palmerton <elwyn.palmerton@gmail.com>
2021-04-16 22:32:55 -0400

    Fix detached head

--- PizzaBox.Client/Singleton/StoreSingleton.cs
+++ PizzaBox.Client/Singleton/StoreSingleton.cs
@@ -7,13 +7,11 @@ namespace PizzaBox.Client.Singletons
 
     public class StoreSingleton
     {
-
         public List<AStore> Stores {get;}
+
         private static StoreSingleton _instance;
 
-        
 
-   
         public static StoreSingleton Instance
         {
             get
@@ -22,53 +20,28 @@ namespace PizzaBox.Client.Singletons
                 {
                     _instance = new StoreSingleton();
                 }
+                return _instance;
             }
-
-            return _instance;
         }
 
-         private StoreSingleton() {
+        private StoreSingleton()
+        {
             if (Stores == null)
             {
-                Stores = Stores(){
-
-                }
+                Stores = new List<AStore>
+                {
+                    new ChicagoStore(),
+                    new NewYorkStore(),
+                };
             }
         }
-
-
-
-  }
-
-
-
-//Then in the program file call StoreSingleton.stores in the foreach loop.
-
-// foreach (AStore item in StoreSingleton.stores)
-
-
-
-// 
-// // serialize data to a file.
-// 
-// //1. Need a file to access.
-// 
-// var path = @"store.xml";
-// 
-// //2. Open the file3. 
-// 
-// var writer = new StreamWriter(path);
-// 
-// // 3. Convert the object to text
-// 
-// var xml = new XmlSerializer(typeof(List<AStore>));
-// 
-// // 4. Write text to the file.
-// 
-// xml.Serialize(writer, Stores);
-//   
-// // 5. Close the file.
-// 
-// writer.Close();
-
+    }
 }
+
+
+
+
+
+
+
+
